47fa0999d503ba220b4af2569d872e25386c3b93
Saptarshi Mondal <smzaptarshi@gmail.com>
2023-06-15 14:48:07 +0530

    cleanup

--- common/src/server/UserCosmosClient.ts
+++ common/src/server/UserCosmosClient.ts
@@ -5,48 +5,13 @@ export default class UserCosmosClient extends BaseCosmosClient<User>{
   constructor() {
     super(User)
     this.containerName = process.env.COSMOSDB_USERS_CONTAINER as string
-    console.log(this.containerName)
   }
 
   createOrUpdateUser = async (user: User): Promise<User> => {
-    console.log("inside create")
     return (await super.createOrUpdate(user)) as User
   }
 
   getUsers = async (): Promise<User[]> => {
     return (await super.getAll()) as User[]
   }
-  // private readonly dataBaseName = process.env.DATABASE_NAME as string
-
-  // private readonly containerName = process.env
-  //   .COSMOSDB_USERS_CONTAINER as string
-
-  // private get container() {
-  //   return appCosmosClient.client
-  //     .database(this.dataBaseName)
-  //     .container(this.containerName)
-  // }
-
-  // createOrUpdateUser = async (user: User): Promise<User> => {
-  //   console.log("inside create")
-  //   await this.container.items.upsert(user)
-  //   return user
-  // }
-
-  // getUsers = async (): Promise<Array<User>> => {
-  //   const { resources } = await this.container.items
-  //     .query({
-  //       query: 'SELECT * FROM c',
-  //       parameters: [
-  //         { name: '@offset', value: '0' },
-  //         { name: '@limit', value: '50' },
-  //       ],
-  //     })
-  //     .fetchAll()
-
-  //   if (resources.length > 0) {
-  //     return resources
-  //   }
-  //   return []
-  // }
 }
