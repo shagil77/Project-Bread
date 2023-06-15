6b8d8879f6a2e62a0d4541f05c9537fc46ce69e2
Saptarshi Mondal <smzaptarshi@gmail.com>
2023-06-15 11:44:32 +0530

    fixed api

--- web/src/pages/api/user/index.ts
+++ web/src/pages/api/user/index.ts
@@ -5,12 +5,12 @@ import UserCosmosClient from '../../../../../common/src/server/UserCosmosClient'
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<User>
+    res: NextApiResponse<User | User[]>
 ): Promise<void> {
     const { method } = req
     const cosmosClient = new UserCosmosClient()
     if (method === 'GET') {
-        const cosmosClient = new UserCosmosClient()
+        // const cosmosClient = new UserCosmosClient()
         try {
             const data = await cosmosClient.getUsers()
             res.status(200).json(data)
@@ -20,10 +20,10 @@ export default async function handler(
             console.log(e)
         }
     } else if (method === 'POST') {
-        const cosmosClient = new UserCosmosClient()
+        // const cosmosClient = new UserCosmosClient()
         try {
             const user = await cosmosClient.createOrUpdateUser(req.body)
-            res.status(200).json(serialize(User, user))
+            res.status(200).json(user)
         } catch (e) {
             //need to update as per the api
             // eslint-disable-next-line no-console
