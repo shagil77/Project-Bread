6fab59a812386f7a7310b36e6fe91830bd78a504
Himon Sarkar <86182110+himon9@users.noreply.github.com>
2023-06-16 13:49:26 +0530

    designed the register page

--- web/src/pages/register/index.tsx
+++ web/src/pages/register/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
 const RegisterPage: React.FC = () => {
   const [readerId, setReaderId] = useState("");
   const [firstName, setFirstName] = useState("");
@@ -37,95 +38,117 @@ const RegisterPage: React.FC = () => {
     }
   };
 
-  return (
-    <div>
-      <h1>Register Page</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="readerId">Reader ID:</label>
-          <input
-            type="text"
-            id="readerId"
-            value={readerId}
-            onChange={(e) => setReaderId(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            type="text"
-            id="firstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="middleName">Middle Name:</label>
-          <input
-            type="text"
-            id="middleName"
-            value={middleName}
-            onChange={(e) => setMiddleName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            type="text"
-            id="lastName"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="gender">Gender:</label>
-          <input
-            type="text"
-            id="gender"
-            value={gender}
-            onChange={(e) => setGender(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="dateOfBirth">Date of Birth:</label>
-          <input
-            type="text"
-            id="dateOfBirth"
-            value={dateOfBirth}
-            onChange={(e) => setDateOfBirth(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="school">School:</label>
-          <input
-            type="text"
-            id="school"
-            value={school}
-            onChange={(e) => setSchool(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="readerType">Reader Type:</label>
-          <input
-            type="text"
-            id="readerType"
-            value={readerType}
-            onChange={(e) => setReaderType(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="profilePicture">Profile Picture:</label>
-          <input
-            type="text"
-            id="profilePicture"
-            value={profilePicture}
-            onChange={(e) => setProfilePicture(e.target.value)}
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
-    </div>
-  );
-};
+return (
+  <div>
+    <h1 style={{ textAlign: "center" }}>Register Page</h1>
+    <form onSubmit={handleSubmit} style={{ maxWidth: "400px", margin: "0 auto" }}>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="readerId">Reader ID:</label>
+        <input
+          type="text"
+          id="readerId"
+          value={readerId}
+          onChange={(e) => setReaderId(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="middleName">Middle Name:</label>
+        <input
+          type="text"
+          id="middleName"
+          value={middleName}
+          onChange={(e) => setMiddleName(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="gender">Gender:</label>
+        <input
+          type="text"
+          id="gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="dateOfBirth">Date of Birth:</label>
+        <input
+          type="text"
+          id="dateOfBirth"
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="school">School:</label>
+        <input
+          type="text"
+          id="school"
+          value={school}
+          onChange={(e) => setSchool(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="readerType">Reader Type:</label>
+        <input
+          type="text"
+          id="readerType"
+          value={readerType}
+          onChange={(e) => setReaderType(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <div style={{ marginBottom: "10px" }}>
+        <label htmlFor="profilePicture">Profile Picture:</label>
+        <input
+          type="text"
+          id="profilePicture"
+          value={profilePicture}
+          onChange={(e) => setProfilePicture(e.target.value)}
+          style={{ width: "100%", padding: "5px" }}
+        />
+      </div>
+      <button
+        type="submit"
+        style={{
+          padding: "10px",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          border: "none",
+          cursor: "pointer",
+          width: "100%",
+        }}
+      >
+        Register
+      </button>
+    </form>
+  </div>
+)
+      }
+
 
 export default RegisterPage;
