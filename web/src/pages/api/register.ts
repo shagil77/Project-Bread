7ec6bb7bfdc11fb334c82527ea2ca02a0d8650ff
Himon Sarkar <86182110+himon9@users.noreply.github.com>
2023-06-16 12:42:10 +0530

    api for new user

---
+++ web/src/pages/api/register.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+import type { NextApiRequest, NextApiResponse } from 'next'
+import User from '../../../../common/dist/model/User'
+import UserCosmosClient from '../../../../common/dist/server/UserCosmosClient'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<User | User[]>
+): Promise<void> {
+    const { method } = req
+    const cosmosClient = new UserCosmosClient()
+    
+    if (method === 'POST') {
+        console.log('hello')
+        // const cosmosClient = new UserCosmosClient()
+        try {
+            const user = User.CreateNew(req.body)
+            console.log(user)
+            await cosmosClient.createOrUpdateUser(user)
+            res.status(200).send(user)
+        } catch (e) {
+            //need to update as per the api
+            // eslint-disable-next-line no-console
+            console.log(e)
+        }
+    } else {
+        res.status(405).end()
+    }
+}
